14bd6275065190b90f59f0dd2a5398ee608dcaf6
ChustrupFx <42353254+ChustrupFx@users.noreply.github.com>
2021-01-03 01:45:49 -0300

    View system ready

--- app/controllers/ChatController.php
+++ app/controllers/ChatController.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Core\View;
+
 class ChatController {
 
     public function view() {
 
-        
+        View::view('index', [
+            'name' => 'Victor'
+        ]);
 
     }
 
--- app/core/View.php
+++ app/core/View.php
@@ -4,8 +4,14 @@ namespace App\Core;
 
 class View {
 
-    static function view($view, $data) {
+    static function view($view, $data = []) {
         
+        $viewFile = "app/views/$view.php";
+
+        if (!file_exists($viewFile))
+            throw new \Exception("The view $view does not exist");
+
+        require_once($viewFile);
 
     }
 
